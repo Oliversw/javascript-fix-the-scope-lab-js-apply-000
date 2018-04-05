599fb1cc9115407101a0325c84dae6c7a6fe6c4c
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2018-04-05 20:18:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ function myAnimal() {
   return animal;
 }
 
-var yourAnimal = 'cat';
-
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  var yourAnimal = 'cat';
   return yourAnimal;
 }
 
